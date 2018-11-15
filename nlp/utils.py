3e811eaf3ea81262184b6fed3badd947d1830d76
Ændrew Rininsland <andrew.rininsland@ft.com>
2018-11-15 15:16:42 +0000

    Fixing issue not passing strings

--- nlp/utils.py
+++ nlp/utils.py
@@ -1,6 +1,7 @@
 from pycorenlp import StanfordCoreNLP
 import os
 
+
 def annotate_corenlp(text, annotators=['pos'], output_format='json', hostname='localhost', port=9000):
     """
     Helper function to get the CoreNLP output.
@@ -15,44 +16,21 @@ def annotate_corenlp(text, annotators=['pos'], output_format='json', hostname='l
         http://stanfordnlp.github.io/CoreNLP/annotators.html. You just need to
         put the property name from that table into this list. (Ex: 'pos', 'ner')
     """
-    try:
-        if type(text) is unicode:
-            UNICODE_ASCII_MAP = {
-                0x2018: u'\'',
-                0x2019: u'\'',
-                0x201c: u'\"',
-                0x201d: u'\"'
-            }
-            text = text.translate(UNICODE_ASCII_MAP).encode(
-                'ascii', 'ignore')
-            # To replace double quotes with single quotes
-            text = text.replace("''", '"')
-            nlp = StanfordCoreNLP('http://{}:{}'.format(
-                os.getenv('CORENLP_HOSTNAME', hostname),
-                os.getenv('CORENLP_PORT', port)))
+    UNICODE_ASCII_MAP = {
+        0x2018: u'\'',
+        0x2019: u'\'',
+        0x201c: u'\"',
+        0x201d: u'\"'
+    }
+    text = text.translate(UNICODE_ASCII_MAP).encode(
+        'ascii', 'ignore')
+    # To replace double quotes with single quotes
+    # text = text.replace("''", '"')
+    nlp = StanfordCoreNLP('http://{}:{}'.format(
+        os.getenv('CORENLP_HOSTNAME', hostname),
+        os.getenv('CORENLP_PORT', port)))
 
-            return nlp.annotate(text, properties={
-                'annotators': ','.join(annotators),
-                'outputFormat': output_format
-                })
-    except Exception:  # Python3
-        if type(text) is str:
-            UNICODE_ASCII_MAP = {
-                0x2018: u'\'',
-                0x2019: u'\'',
-                0x201c: u'\"',
-                0x201d: u'\"'
-            }
-            text = text.translate(UNICODE_ASCII_MAP).encode(
-                'ascii', 'ignore')
-            # To replace double quotes with single quotes
-            text = text.replace(b"''", b'"')
-
-            nlp = StanfordCoreNLP('http://{}:{}'.format(
-                os.getenv('CORENLP_HOSTNAME', hostname),
-                os.getenv('CORENLP_PORT', port)))
-
-            return nlp.annotate(str(text), properties={
-                'annotators': ','.join(annotators),
-                'outputFormat': output_format
-                })
+    return nlp.annotate(text, properties={
+        'annotators': ','.join(annotators),
+        'outputFormat': output_format
+    })
